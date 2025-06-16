746d3a7cc619e2ca55d91d4d811c60497705c69b
seha9631 <gktkddms9631@gmail.com>
2025-06-16 22:43:24 +0900

    feat(router): 라우팅 구조 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 
 import './App.css'
+import Home from './pages/Home'
+import Signup from "./pages/Signup";
+import Search from "./pages/Search";
+import Carts from "./pages/Carts";
+import CoureseList from "./pages/Course";
+import CourseDetail from "./pages/Courses";
+import FindPassword from "./pages/signin/find/Password";
+import FindId from "./pages/signin/find/Id";
+import Notfound from './pages/Notfound';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("carts");
+  }
 
   return (
     <>
-
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/courese"}>CoureseList</Link>
+        <Link to={"/courese/1"}>Courese1</Link>
+        <Link to={"/signup"}>Signup</Link>
+        <Link to={"/signin/find/password"}>FindPassword</Link>
+        <Link to={"/signin/find/id"}>FindId</Link>
+        <Link to={"/"}>Home</Link>
+      </div>
+      <button onClick={onClickButton}>
+        장바구니
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courese" element={<CoureseList />} />
+        <Route path="/courese/:id" element={<CourseDetail />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/carts" element={<Carts />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin/find/password" element={<FindPassword />} />
+        <Route path="/signin/find/id" element={<FindId />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </>
   )
 }
