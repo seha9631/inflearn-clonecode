e756e89f310dffda570668d720ad068c3334d3ec
seha9631 <gktkddms9631@gmail.com>
2025-06-17 02:46:59 +0900

    feat(layout): TopBar 공통 레이아웃 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ import FindPassword from "./pages/signin/find/Password";
 import FindId from "./pages/signin/find/Id";
 import Notfound from './pages/Notfound';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import TopBar from './layout/TopBar';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 function App() {
   const nav = useNavigate();
@@ -19,31 +22,34 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/courese"}>CoureseList</Link>
-        <Link to={"/courese/1"}>Courese1</Link>
-        <Link to={"/signup"}>Signup</Link>
-        <Link to={"/signin/find/password"}>FindPassword</Link>
-        <Link to={"/signin/find/id"}>FindId</Link>
-        <Link to={"/"}>Home</Link>
-      </div>
-      <button onClick={onClickButton}>
-        장바구니
-      </button>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courese" element={<CoureseList />} />
-        <Route path="/courese/:id" element={<CourseDetail />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/carts" element={<Carts />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin/find/password" element={<FindPassword />} />
-        <Route path="/signin/find/id" element={<FindId />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </>
+    <MantineProvider>
+      <>
+        <TopBar />
+        <div>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/courese"}>CoureseList</Link>
+          <Link to={"/courese/1"}>Courese1</Link>
+          <Link to={"/signup"}>Signup</Link>
+          <Link to={"/signin/find/password"}>FindPassword</Link>
+          <Link to={"/signin/find/id"}>FindId</Link>
+          <Link to={"/"}>Home</Link>
+        </div>
+        <button onClick={onClickButton}>
+          장바구니
+        </button>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courese" element={<CoureseList />} />
+          <Route path="/courese/:id" element={<CourseDetail />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/carts" element={<Carts />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin/find/password" element={<FindPassword />} />
+          <Route path="/signin/find/id" element={<FindId />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </>
+    </MantineProvider>
   )
 }
 
