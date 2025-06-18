021f200cd0e9b406f5abcf4c851d08b0414cbe06
seha9631 <gktkddms9631@gmail.com>
2025-06-18 20:51:52 +0900

    feat(navbar): 강의 메뉴에 카테고리 목록 연결 및 동적 렌더링 적용

--- src/layout/Header/NavBarLeft.jsx
+++ src/layout/Header/NavBarLeft.jsx
@@ -1,7 +1,7 @@
 import { Group, Menu, Text, Anchor } from '@mantine/core';
-import { HiOutlineChatAlt, HiOutlineChatAlt2, HiOutlineBookOpen } from 'react-icons/hi';
-import { FaLaptopCode, FaChevronRight, FaRegStar, FaRegLightbulb } from 'react-icons/fa6';
+import { FaChevronRight } from 'react-icons/fa6';
 import { TbCalendarStats } from 'react-icons/tb';
+import { categories } from '../../util/constants';
 import { Link } from 'react-router-dom';
 import classes from './NavBarLeft.module.css';
 
@@ -21,7 +21,7 @@ function NavBarLeft() {
                 <Menu.Target>
                     <Link to='/courses/all' style={{ textDecoration: 'none' }}>
                         <Text
-                            fz={16}
+                            fz='md'
                             fw={700}
                             underline='never'
                             className={classes['HoverGreen']}
@@ -33,38 +33,21 @@ function NavBarLeft() {
 
                 </Menu.Target>
                 <Menu.Dropdown className={classes['SharpBorder']}>
-                    <Menu.Sub width={180}>
-                        <Menu.Sub.Target>
-                            <Menu.Sub.Item rightSection>개발 · 프로그래밍</Menu.Sub.Item>
-                        </Menu.Sub.Target>
-
-                        <Menu.Sub.Dropdown
-                            className={classes['SharpBorder']}
-                            style={{
-                                marginTop: `${0 * -35 - 5}px`,
-                                marginLeft: '4px'
-                            }}
-                        >
-                            <Menu.Item>웹 개발</Menu.Item>
-                            <Menu.Item>프론트엔드</Menu.Item>
-                        </Menu.Sub.Dropdown>
-                    </Menu.Sub>
-                    <Menu.Sub width={180}>
-                        <Menu.Sub.Target>
-                            <Menu.Sub.Item rightSection>게임 개발</Menu.Sub.Item>
-                        </Menu.Sub.Target>
-
-                        <Menu.Sub.Dropdown
-                            className={classes['SharpBorder']}
-                            style={{
-                                marginTop: `${1 * -35 - 5}px`,
-                                marginLeft: '4px'
-                            }}
-                        >
-                            <Menu.Item>웹 개발</Menu.Item>
-                            <Menu.Item>프론트엔드</Menu.Item>
-                        </Menu.Sub.Dropdown>
-                    </Menu.Sub>
+                    {categories.filter((cat) => cat.value !== 'all')
+                        .map((cat) => (
+                            <Menu.Item key={cat.value}>
+                                <Link to={`/courses/${cat.value}`} style={{ textDecoration: 'none' }}>
+                                    <Text
+                                        c="black"
+                                        fz="sm"
+                                        fw={400}
+                                        underline="never"
+                                    >
+                                        {cat.label}
+                                    </Text>
+                                </Link>
+                            </Menu.Item>
+                        ))}
                     <Menu.Divider />
                     <Menu.Item
                         leftSection={<TbCalendarStats size={18} />}
@@ -77,7 +60,7 @@ function NavBarLeft() {
 
             <Anchor
                 href='https://www.inflearn.com/roadmaps'
-                fz={16}
+                fz='md'
                 fw={700}
                 underline='never'
                 className={classes['HoverGreen']}
@@ -86,7 +69,7 @@ function NavBarLeft() {
             </Anchor>
             <Anchor
                 href='https://mentoring.inflearn.com/mentors'
-                fz={16}
+                fz='md'
                 fw={700}
                 underline='never'
                 className={classes['HoverGreen']}
@@ -113,7 +96,7 @@ function NavBarLeft() {
                         style={{ textDecoration: 'none' }}
                     >
                         <Text
-                            fz={16}
+                            fz='md'
                             fw={700}
                             underline='never'
                             className={classes['HoverGreen']}
@@ -123,41 +106,22 @@ function NavBarLeft() {
                     </Link>
                 </Menu.Target>
                 <Menu.Dropdown>
-                    <Menu.Item
-                        leftSection={<HiOutlineChatAlt2 size={14} />}
-                    >
-                        <Anchor
-                            href='https://www.inflearn.com/courses?types=ONLINE'
-                            fz={14}
-                            underline='never'
-                            className={classes['HoverBlack']}
-                        >
-                            질문 & 답변
-                        </Anchor>
+                    <Menu.Item>
+                        질문 & 답변
                     </Menu.Item>
-                    <Menu.Item
-                        leftSection={<HiOutlineChatAlt size={14} />}
-                    >
+                    <Menu.Item>
                         고민있어요
                     </Menu.Item>
-                    <Menu.Item
-                        leftSection={<HiOutlineBookOpen size={14} />}
-                    >
+                    <Menu.Item>
                         스터디
                     </Menu.Item>
-                    <Menu.Item
-                        leftSection={<FaLaptopCode size={14} />}
-                    >
+                    <Menu.Item>
                         팀 프로젝트
                     </Menu.Item>
-                    <Menu.Item
-                        leftSection={<FaRegStar size={14} />}
-                    >
+                    <Menu.Item>
                         수강평
                     </Menu.Item>
-                    <Menu.Item
-                        leftSection={<FaRegLightbulb size={14} />}
-                    >
+                    <Menu.Item>
                         멘토링 후기
                     </Menu.Item>
                 </Menu.Dropdown>
