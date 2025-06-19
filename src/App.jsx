054ad1c31500bdd29be9fcdcae1196a16491ee6e
seha9631 <gktkddms9631@gmail.com>
2025-06-19 17:30:14 +0900

    feat(header): 로그인한 사용자 정보 기반으로 장바구니와 유저 메뉴 렌더링 적용

--- src/App.jsx
+++ src/App.jsx
@@ -20,19 +20,6 @@ import Questions from './pages/community/questions';
 import { useState } from 'react';
 
 function App() {
-  const userId = 'seha';
-  const userInfo = {
-    password: '9631',
-    phoneNumber: '01011119631',
-    name: '하상은',
-    age: 27,
-    cart: ['김호-리더십-메시지-메이커', '기획자와-pm을-위한-artificial-intelligence-강의”, "기초-대수학-중고등'],
-    wishlist: ['프롬프트-엔지니어링-챗지피티'],
-    enrolled: ['한입-크기-타입스크립트', 'typescript-react-perfect-course'],
-    coupon: ['INFCON2024 전용 쿠폰'],
-    point: 12000
-  };
-  localStorage.setItem(userId, JSON.stringify(userInfo));
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
